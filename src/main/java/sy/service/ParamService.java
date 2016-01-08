f7a3a19a601e2061b6c109c253130433861eea3b
heyh <yhua.he@163.com>
2016-01-08 15:58:17 +0800

    单位

--- src/main/java/sy/service/ParamService.java
+++ src/main/java/sy/service/ParamService.java
@@ -1,13 +1,9 @@
 package sy.service;
 
-import sy.model.Param;
-
-import java.util.List;
-
 /**
  * Created by heyh on 16/1/6.
  */
 public interface ParamService {
 
-    public List<Param> getUnitParams();
+    public Object getParams(String key, String value);
 }
