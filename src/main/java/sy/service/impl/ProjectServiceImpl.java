da0a2436b11782db47fd44f70c89d9ea8c84c6a7
heyh <yhua.he@163.com>
2016-01-19 17:14:24 +0800

    部门费用类型

--- src/main/java/sy/service/impl/ProjectServiceImpl.java
+++ src/main/java/sy/service/impl/ProjectServiceImpl.java
@@ -98,7 +98,7 @@ public class ProjectServiceImpl implements ProjectServiceI {
 			}
 			hql.append(") ");
 		}
-		hql.append(" and p.isdel = 0 and p.isLock = '0' order by p.id desc");
+		hql.append(" and p.isdel = 0 order by p.id desc");
 		return hql.toString();
 
 	}
@@ -275,7 +275,7 @@ public class ProjectServiceImpl implements ProjectServiceI {
             hql.append(" and compId = :compId");
             params.put("compId", compId);
         }
-        hql.append(" and p.isdel = 0 and p.isLock = '0' order by p.id desc");
+        hql.append(" and p.isdel = 0 order by p.id desc");
         return hql.toString();
 
     }
