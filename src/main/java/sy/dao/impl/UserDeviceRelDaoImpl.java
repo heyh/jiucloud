242f89f60700e8c9688f32f81f791195780a270e
heyh <yhua.he@163.com>
2017-07-13 23:31:23 +0800

    execl

--- src/main/java/sy/dao/impl/UserDeviceRelDaoImpl.java
+++ src/main/java/sy/dao/impl/UserDeviceRelDaoImpl.java
@@ -1,10 +1,12 @@
 package sy.dao.impl;
 
+import org.springframework.stereotype.Repository;
 import sy.dao.UserDeviceRelDaol;
 import sy.model.po.UserDeviceRel;
 
 /**
  * Created by heyh on 2017/7/13.
  */
+@Repository
 public class UserDeviceRelDaoImpl extends BaseDaoImpl<UserDeviceRel> implements UserDeviceRelDaol {
 }
